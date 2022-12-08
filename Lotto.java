2590b62cb7ecfd15b84f7b4016369b8304d434b4
Faragó Csaba <valami@valami.hu>
2022-12-08 17:30:31 +0100

    Lotto+

--- Lotto.java
+++ Lotto.java
@@ -1,5 +1,10 @@
 public class Lotto{
     public static void main(String[] args) {
         int[] lottoszamok = new int[5];
+        lottoszamok[0] = 34;
+        lottoszamok[1] = 7;
+        lottoszamok[2] = 83;
+        lottoszamok[3] = 28;
+        lottoszamok[4] = 69;
     }
 }

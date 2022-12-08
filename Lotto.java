be9f4e16c37ed21d3ee2ad4e2ec09ae925050b74
Faragó Csaba <valami@valami.hu>
2022-12-08 17:38:07 +0100

    Lotto

--- Lotto.java
+++ Lotto.java
@@ -1,7 +1,7 @@
 public class Lotto{
     public static void main(String[] args) {
-        int[] lottoszamok = new int[5];
-        lottoszamok[0] = 34;
+        int[] lottoszamok = new int[5]; //létrehozunk egy tömböt, öt elemmel
+        lottoszamok[0] = 34;            //feltöltjük a tömböt, a 0 az az index, a tömb mutatója hogy melyik elemnél van
         lottoszamok[1] = 7;
         lottoszamok[2] = 83;
         lottoszamok[3] = 28;
